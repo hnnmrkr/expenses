d1b27fc1d8bf0caab318e7563e15dbedfa0fa3b2
Hanna <hanna-marii.kriisa@voco.ee>
2023-01-10 11:41:52 +0200

    Use JS logic in component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,10 @@
 import './ExpenseItem.css'
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props){
-    const day = props.expenseData.date.toLocaleString('en-US', {month:'long'})
-    const month = props.expenseData.date.toLocaleString('en-US', {day:'2-digit'})
-    const year = props.expenseData.date.getFullYear()
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{day}</div>
-                <div>{year}</div>
-            </div>
+        <ExpenseDate date={props.expenseData.date}></ExpenseDate>
             <div className="expense-item__description">
                 <h2>{props.expenseData.title}</h2>
                 <div className="expense-item__price">{props.expenseData.price}€</div>
